d0b687f063cab131afa8157d862aa3c36482e898
Maria Panagos <maria.pan0330@gmail.com>
2023-04-17 14:30:53 -0500

    writes to a txt file now too & fixed edge cases

--- main.py
+++ main.py
@@ -3,31 +3,34 @@
 from reportlab.pdfgen import canvas
 from reportlab.platypus import Paragraph
 from reportlab.lib.pagesizes import A4
-import os
-import time
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+
+
+cover_letter_msg = """Dear {manager}, {newline}{newline}
+
+I’m happy to see that the position of {role} is available at {company}! As a full-stack developer with experience in Java and Python, I am excited to put my skills to use maintaining and improving the user experience you have perfected. {newline}{newline}
+
+My experience in full-stack development specializing in Python, JavaScript, SQL, PostgreSQL, Flask, and React makes me an ideal candidate for this role. I love the problem-solving process of learning new things and fine-tuning my skills, and I consistently ensure a thorough design that accounts for edge cases and provides an intuitive end-user experience. {newline}{newline}
+
+I value the knowledge gained through diverse experiences, and I believe I provide a unique skill set to the teams you are building. I graduated from Trinity College Dublin with a BA in Psychology, with a minor in Sociology. I also had the opportunity to travel in Europe as an international student and to practice public relations and communications in my position as the PR officer in a college society. {newline}{newline}
+
+These experiences, along with my coding experience and personal projects leave me in an optimal position to both learn from my participation and benefit your team at {company}. {newline}{newline}
+
+Sincerely,{newline}
+Maria Panagos"""
+
+def make_cover_letter_txt(manager, role, company):
+    with open("./Cover Letters/coverLetter.txt", "w") as file:
+        file.write(cover_letter_msg.format(manager=manager, role=role, company=company, newline=""))
+
 
 
 def make_cover_letter(manager, role, company):
     # make a new canvas to draw on at the specified relative location, and with the A4 size.
     width,height=A4
     c = canvas.Canvas("./Cover Letters/Cover Letter.pdf", pagesize=A4)
-    if manager == '':
-        manager = "Hiring Manager"
-    p1=Paragraph(f"""Dear {manager}, <br/><br/>
-
-    I’m happy to see that the position of {role} is available at {company}! As a full-stack developer with experience in Java and Python, I am excited to put my skills to use maintaining and improving the user experience you have perfected. <br/><br/>
-
-    My experience in full-stack development specializing in Python, JavaScript, SQL, PostgreSQL, Flask, and React makes me an ideal candidate for this role. I love the problem-solving process of learning new things and fine-tuning my skills, and I consistently ensure a thorough design that accounts for edge cases and provides an intuitive end-user experience. <br/><br/>
-
-    I value the knowledge gained through diverse experiences, and I believe I provide a unique skill set to the teams you are building. I graduated from Trinity College Dublin with a BA in Psychology, with a minor in Sociology. I also had the opportunity to travel in Europe as an international student and to practice public relations and communications in my position as the PR officer in a college society. <br/><br/>
-
-    These experiences, along with my coding experience and personal projects leave me in an optimal position to both learn from my participation and benefit your team at {company}. <br/><br/>
-
-    Sincerely,<br/>
-    Maria Panagos
-    """)
+    p1=Paragraph(cover_letter_msg.format(manager=manager, role=role, company=company, newline="<br/>"))
     p1.wrapOn(c,500,450)
     p1.drawOn(c,width-550,height-290)
 
@@ -68,12 +71,16 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
 
     def submit_info(self):
-        manager = self.manager_input.text().title()
-        role = self.role_input.text()
-        company = self.company_input.text()
+        manager = self.manager_input.text().strip().title()
+        if manager == '':
+            manager = "Hiring Manager"
+        role = self.role_input.text().strip()
+        company = self.company_input.text().strip()
+
         print(f"Manager: {manager}\nRole: {role}\nCompany: {company}")
         self.submission_summary.setText(f"SUBMITTED\nManager: {manager}\nRole: {role}\nCompany: {company}")
         make_cover_letter(manager, role, company)
+        make_cover_letter_txt(manager, role, company)
 
 
 if __name__ == "__main__":
